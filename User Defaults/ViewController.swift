279e9f2802ce6eaae8d774a3bfc6fae28f0cdf31
Suraj Jaiswal <suraj.jaiswal@docquity.com>
2023-02-09 14:22:11 +0530

    Data Saved & Retrieved

--- User Defaults/ViewController.swift
+++ User Defaults/ViewController.swift
@@ -8,12 +8,86 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let defaults = UserDefaults.standard
+    
+    var isDarkMode : Bool = false
+    var name : String = ""
 
+    @IBOutlet weak var ModeToggler: UISegmentedControl!
+    @IBOutlet weak var EditText: UITextField!
+    @IBOutlet weak var SaveBtn: UIButton!
+    
+
+    // ------------ User Default Keys ------------
+
+    struct Keys {
+        static let prefDarkMode = "prefDarkMode"
+        static let prefSavedName = "prefSavedName"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        chkPref()
     }
 
+    // ------------ Action Handlers ------------
 
+    @IBAction func SaveBtn(_ sender: Any) {
+        
+        isDarkMode = ModeToggler.selectedSegmentIndex == 1
+        saveStylePref()
+        
+        name = EditText.text ?? ""
+        if (!name.isEmpty){
+            savePrefName()
+        }
+        
+    }
+    
+    @IBAction func modeToggler(_ sender: UISegmentedControl) {
+        isDarkMode = ModeToggler.selectedSegmentIndex == 1
+        updateStyle()
+    }
+    
+    // ------------ Saving view elements preferences ------------
+    
+    func saveStylePref(){
+        defaults.set(isDarkMode, forKey: Keys.prefDarkMode)
+    }
+    
+    func savePrefName(){
+        defaults.set(name, forKey: Keys.prefSavedName)
+    }
+    
+    // ------------ Checking Preferences ------------
+    
+    func chkPref(){
+        isDarkMode = defaults.bool(forKey: Keys.prefDarkMode)
+        if (isDarkMode){
+            ModeToggler.selectedSegmentIndex = 1
+        }
+        updateStyle()
+        
+        name = defaults.string(forKey: Keys.prefSavedName) ?? ""
+        print(name)
+        updateName()
+        
+    }
+    
+    // ------------ updating view elements ------------
+    
+    func updateStyle(){
+        if(isDarkMode){
+            self.view.backgroundColor = UIColor.black
+        }else{
+            self.view.backgroundColor = UIColor.white
+        }
+    }
+    
+    func updateName(){
+        EditText.text = name
+    }
+     
 }
 
